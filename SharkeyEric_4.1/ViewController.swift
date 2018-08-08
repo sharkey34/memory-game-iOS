23c564665ec4f2a1eaaadd703839856c39a3b809
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-08-08 15:34:46 -0400

    Before changing stackView

--- SharkeyEric_4.1/ViewController.swift
+++ SharkeyEric_4.1/ViewController.swift
@@ -11,19 +11,40 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-    @IBOutlet var imageCollection: [UIImageView]!
+    @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var movesLabel: UILabel!
+    @IBOutlet var iPhoneCollection: [UIImageView]!
     
     var player = AVAudioPlayer()
     var iPhoneImages: [[UIImage]] = [[#imageLiteral(resourceName: "misc space rocket"),#imageLiteral(resourceName: "monetary gold bars"),#imageLiteral(resourceName: "dressup lips"),#imageLiteral(resourceName: "emoticons crush"),#imageLiteral(resourceName: "emoticons laughing out loud"),#imageLiteral(resourceName: "music speaker"),#imageLiteral(resourceName: "magic ripped eye"),#imageLiteral(resourceName: "monster zombie2"),#imageLiteral(resourceName: "monster brain"),#imageLiteral(resourceName: "magic triangle flask"),#imageLiteral(resourceName: "misc space rocket"),#imageLiteral(resourceName: "monetary gold bars"),#imageLiteral(resourceName: "dressup lips"),#imageLiteral(resourceName: "emoticons crush"),#imageLiteral(resourceName: "emoticons laughing out loud"),#imageLiteral(resourceName: "music speaker"),#imageLiteral(resourceName: "magic ripped eye"),#imageLiteral(resourceName: "monster zombie2"),#imageLiteral(resourceName: "monster brain"),#imageLiteral(resourceName: "magic triangle flask")],[#imageLiteral(resourceName: "casino dice"),#imageLiteral(resourceName: "casino dice"),#imageLiteral(resourceName: "casino token"),#imageLiteral(resourceName: "casino token"),#imageLiteral(resourceName: "minerals blue stone"),#imageLiteral(resourceName: "minerals blue stone"),#imageLiteral(resourceName: "minerals red stone"),#imageLiteral(resourceName: "minerals red stone"),#imageLiteral(resourceName: "music microphone"),#imageLiteral(resourceName: "music microphone")]]
+    var images: [[UIImage]] = [[#imageLiteral(resourceName: "misc space rocket"),#imageLiteral(resourceName: "monetary gold bars"),#imageLiteral(resourceName: "dressup lips"),#imageLiteral(resourceName: "emoticons crush"),#imageLiteral(resourceName: "emoticons laughing out loud"),#imageLiteral(resourceName: "music speaker"),#imageLiteral(resourceName: "magic ripped eye"),#imageLiteral(resourceName: "monster zombie2"),#imageLiteral(resourceName: "monster brain"),#imageLiteral(resourceName: "magic triangle flask"),#imageLiteral(resourceName: "misc space rocket"),#imageLiteral(resourceName: "monetary gold bars"),#imageLiteral(resourceName: "dressup lips"),#imageLiteral(resourceName: "emoticons crush"),#imageLiteral(resourceName: "emoticons laughing out loud"),#imageLiteral(resourceName: "music speaker"),#imageLiteral(resourceName: "magic ripped eye"),#imageLiteral(resourceName: "monster zombie2"),#imageLiteral(resourceName: "monster brain"),#imageLiteral(resourceName: "magic triangle flask")],[#imageLiteral(resourceName: "casino dice"),#imageLiteral(resourceName: "casino dice"),#imageLiteral(resourceName: "casino token"),#imageLiteral(resourceName: "casino token"),#imageLiteral(resourceName: "minerals blue stone"),#imageLiteral(resourceName: "minerals blue stone"),#imageLiteral(resourceName: "minerals red stone"),#imageLiteral(resourceName: "minerals red stone"),#imageLiteral(resourceName: "music microphone"),#imageLiteral(resourceName: "music microphone")]]
     var selectedImage: [Int] = []
     var imageArray: [UIImage] = []
     var numMoves: Int = 0
+    var device = ""
+    var numImages = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        for image in imageCollection{
-            image.backgroundColor = UIColor.cyan
+        
+        switch UIDevice.current.userInterfaceIdiom {
+        case .phone:
+            device = "phone"
+            numImages = 19
+        case .pad:
+            device = "pad"
+            numImages = 29
+        default:
+            print("Device Unsepecified.")
+        }
+        
+        playButton.titleLabel?.adjustsFontSizeToFitWidth = true
+        movesLabel.text = ""
+        var counter = 0
+        for image in iPhoneCollection[...numImages]{
+            counter += 1
+        print(counter)
+            image.backgroundColor = UIColor.black
             image.layer.cornerRadius = 10
         }
     }
@@ -42,29 +63,29 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         case 0:
             selectedImage.append(imageView.tag)
             imageView.isUserInteractionEnabled = false
-            imageCollection[imageView.tag].image = imageArray[imageView.tag]
+            iPhoneCollection[imageView.tag].image = imageArray[imageView.tag]
         //MARK: Show image when clicked.
         case 1:
             numMoves += 1
             movesLabel.text = "Moves: \(numMoves)"
-            imageCollection[imageView.tag].image = imageArray[imageView.tag]
             selectedImage.append(imageView.tag)
             imageView.isUserInteractionEnabled = false
+            iPhoneCollection[imageView.tag].image = imageArray[imageView.tag]
             //MARK: Show image clicked.
             if imageArray[selectedImage[0]] == imageArray[selectedImage[1]] {
                 
                 playAudio(resource: "correct", type: "wav")
                 //MARK: Play correct sound.
                 for index in selectedImage{
-                    imageCollection[index].backgroundColor = UIColor.black
-                    imageCollection[index].image = nil
+                    iPhoneCollection[index].backgroundColor = UIColor.black
+                    iPhoneCollection[index].image = nil
                 }
             } else {
                 //MARK: Play incorrect sound and flip cards back over.
                 playAudio(resource: "incorrect", type: "wav")
                 for index in selectedImage{
-                    imageCollection[index].isUserInteractionEnabled = true
-                    imageCollection[index].image = nil
+                    iPhoneCollection[index].isUserInteractionEnabled = true
+                    iPhoneCollection[index].image = nil
                 }
                 print("You suck")
             }
@@ -78,7 +99,8 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     @IBAction func playButtonSelected(_ sender: UIButton) {
         // For loop to add an UITapGestureRecognizer to each imageView and set the user interaction to true and adding images.
         if sender.titleLabel?.text == "Play"{
-        for image in imageCollection{
+        for image in iPhoneCollection{
+            image.backgroundColor = UIColor.cyan
             //MARK: Add countdown.
             //MARK: Turn play button to a stop button and reset the playing field.
                 
@@ -95,24 +117,30 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
             } else {
                 let index = arc4random_uniform(UInt32(iPhoneImages[0].count))
                 image.image = iPhoneImages[0][Int(index)]
-                imageArray.append(iPhoneImages[0][Int(index)])
+                imageArray.append(image.image!)
                 iPhoneImages[0].remove(at: Int(index))
             }
                 playAudio(resource: "countdown", type: "wav")
         }
                   sender.setTitle("Stop", for: .normal)
         } else if sender.titleLabel?.text == "Stop"{
-            for image in imageCollection{
+            for image in iPhoneCollection{
                 image.image = nil
                 image.backgroundColor = UIColor.black
             }
+            
+            iPhoneImages = images
+            numMoves = 0
+            selectedImage = []
+            imageArray = []
+            movesLabel.text = "Moves:"
             sender.setTitle("Play", for: .normal)
         }
     }
     
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        for image in imageCollection{
+        for image in iPhoneCollection{
             image.image = nil
         }
     }
