53e4a9cfb2ca184ec14d7e2bf5a0281ff979eac6
Eric Sharkey <eisharkey@fullsail.student.edu>
2018-08-22 18:00:02 -0400

    Fixed button constraints for landscape

--- SharkeyEric_4.1/ScoresTableViewController.swift
+++ SharkeyEric_4.1/ScoresTableViewController.swift
@@ -14,26 +14,28 @@ class ScoresTableViewController: UITableViewController {
     var managedContext: NSManagedObjectContext!
     var leaderBoardData: NSManagedObject!
     private var dataObj: [NSManagedObject] = []
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.backgroundColor = UIColor.init(red:0.80, green:0.25, blue:0.25, alpha:1.0)
         navigationController?.isNavigationBarHidden = false
         load()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         tableView.rowHeight = 106
+        tableView.sectionHeaderHeight = 65
         tableView.separatorColor = UIColor.init(displayP3Red: 0.63, green:0.86, blue:1.00, alpha:1.0)
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "LeaderBoard: Top 10"
     }
@@ -44,11 +46,10 @@ class ScoresTableViewController: UITableViewController {
             return dataObj.count
         }
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "scoreCell", for: indexPath) as? ScoresTableViewCell else {return tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)}
- 
         cell.nameLabel.text = dataObj[indexPath.row].value(forKey: "userName") as? String
         let moves = dataObj[indexPath.row].value(forKey: "moves") as? Int
         let date = dataObj[indexPath.row].value(forKey: "date") as? Date
@@ -59,16 +60,16 @@ class ScoresTableViewController: UITableViewController {
             let timeSeconds = t % 60
             cell.timeLabel.text = String(format:"%02i:%02i", timeMinutes, timeSeconds)
         } else {
-           cell.timeLabel.text = "N/A"
+            cell.timeLabel.text = "N/A"
         }
-    
+        
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .short
         if let d = date{
             let dateString = formatter.string(from: d)
             cell.dateLabel.text = dateString
-
+            
         }
         
         let position = indexPath.row + 1
@@ -78,7 +79,7 @@ class ScoresTableViewController: UITableViewController {
         cell.movesLabel.text = moves?.description
         return cell
     }
-
+    
     func load(){
         /* We use Fetch Requests to get the data we want off of the "notepad" */
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "LeaderBoardData")
