589961ffca30a6a973fd63a64902d771ec40cc15
Eric Sharkey <eisharkey@student.fullsail.edu>
2019-01-01 18:09:56 -0500

    ADD - play button to transitions to the start of the game.

---
+++ SharkeyEric_4.1/Views/StartViewController.swift
@@ -0,0 +1,18 @@
+//
+//  StartViewController.swift
+//  SharkeyEric_4.1
+//
+//  Created by Eric Sharkey on 12/29/18.
+//  Copyright © 2018 Eric Sharkey. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
