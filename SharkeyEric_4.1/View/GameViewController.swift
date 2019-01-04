f439a4300d863b2c418bddc69f67d4506bf1fe9f
Eric Sharkey <eisharkey@student.fullsail.edu>
2019-01-04 01:31:41 -0500

    ADD - logic to check the matches when a card is selected. hide cards and flip them back when play is selected again

--- SharkeyEric_4.1/View/GameViewController.swift
+++ SharkeyEric_4.1/View/GameViewController.swift
@@ -20,25 +20,26 @@ class GameViewController: UIViewController {
     
     // Member Variables
     private var viewModel = GameViewModel()
-    private var cards: [Card]?
-    private var imageArray: [UIImage] = []
+    private var selectedCard: Card?
     
     // Core Data variables.
     private var managedContext: NSManagedObjectContext!
     private var entityDescription: NSEntityDescription!
+    private var leaderBoardData: NSManagedObject!
     
-    // Creating variables for the audioPlayer, to hold the images for each device, number of moves, and other information needed in the application.
+    // Audio
     private var player = AVAudioPlayer()
-    private var selectedImage: [Int] = []
+    
+    // Timer
+    private var timer = Timer()
+
+    // counters and labels
     private var moves: Int = 0
     private var numImages = 0
-    private var timer = Timer()
     private var timeSeconds = 0
     private var timeMinutes = 0
     private var time = 0
     private var name = ""
-    private var leaderBoardData: NSManagedObject!
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,8 +55,6 @@ class GameViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    
     
     func setUp(){
         animateCards()
@@ -96,13 +95,10 @@ class GameViewController: UIViewController {
         if sender.titleLabel?.text == "Play" {
             playButton.setTitle("Stop", for: .normal)
             
-            flipFront()
-        
             for card in cardCollection{
                 card.isUserInteractionEnabled = true
+                flipFront(card: card)
             }
-            
-            
         } else if sender.titleLabel?.text == "Stop"{
             playButton.setTitle("Play", for: .normal)
             
@@ -119,20 +115,18 @@ class GameViewController: UIViewController {
     }
     
     // Card flips
-    func flipFront(){
+    func flipFront(card: Card){
         playAudio(resource: "countdown", type: "mp3")
         
-        for card in cardCollection{
             card.setImage(card.cardImage, for: .normal)
             UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromLeft, animations: nil)
-        }
     }
     
+    // Functio to flip card back
     func flipBack(card: Card){
         card.setImage(card.backImage, for: .normal)
         UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight, animations: nil, completion: nil)
     }
-    
     
     // Function to keep track of the time that has passed counter the interval each time it is fired and displaying the time in the label.
     @objc func updateTimer(){
@@ -144,8 +138,15 @@ class GameViewController: UIViewController {
         timerLabel.text = String(format:"%02i:%02i", timeMinutes, timeSeconds)
     }
     
+    // function to stop the the player and reset labels and counters.
     func reset(){
         // Re-setting
+        for card in cardCollection{
+            if card.isHidden == true {
+                card.isHidden = false
+            }
+        }
+        
         player.stop()
         timer.invalidate()
         timeSeconds = 0
@@ -155,154 +156,56 @@ class GameViewController: UIViewController {
         movesLabel.text = nil
         timerLabel.text = nil
     }
-
     
-//    // Function to run when image is tapped.
-//    @objc func imageTapped(sender: UITapGestureRecognizer){
-//        // Checking that the sender is an imageView.
-//        guard let imageView = sender.view as? UIImageView else {return}
-//
-//        // Making sure the image selected is flipped over, Then playing the tap audio
-//        if imageView.image == nil{
-//            playAudio(resource: "tap", type: "mp3")
-//
-//            // if the selected image count is 0 then adding the imageViews tag that was selected to the selected imageArray
-//            // Disabling user interaction and setting the imageViews image to the selected image.
-//            switch selectedImage.count {
-//            case 0:
-//                selectedImage.append(imageView.tag)
-//                imageView.isUserInteractionEnabled = false
-//                imageViewCollection[imageView.tag].image = imageArray[imageView.tag]
-//
-//            // If there already is an image selected then upping the number of moves, setting userInteraction to false and displaying the image associated with that imageView.
-//            case 1:
-//                moves += 1
-//                movesLabel.text = "Moves: \(moves)"
-//                selectedImage.append(imageView.tag)
-//                imageView.isUserInteractionEnabled = false
-//                imageViewCollection[imageView.tag].image = imageArray[imageView.tag]
-//                // Stalling for .5 seconds to display the second card to the user. when the time is up comparing the two selected images.
-//                // if they are correct playing the correct sound and turning the image views over setting the images to nil.
-//                DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
-//                    if self.imageArray[self.selectedImage[0]] == self.imageArray[self.selectedImage[1]] {
-//                        self.playAudio(resource: "correct", type: "wav")
-//                        for index in self.selectedImage{
-//                            self.imageViewCollection[index].backgroundColor = UIColor.init(displayP3Red: 1.00, green:1.00, blue:0.92, alpha:1.0)
-//                            self.imageViewCollection[index].image = nil
-//                        }
-//                        // Checking to see if they are the last two imageViews and display the win message.
-//                        self.alert()
-//                        // If they are not correct playing the incorrect sound and setting the image back to nil.
-//                    } else {
-//                        self.playAudio(resource: "incorrect", type: "wav")
-//                        for index in self.selectedImage{
-//                            self.imageViewCollection[index].isUserInteractionEnabled = true
-//                            self.imageViewCollection[index].image = nil
-//                        }
-//                    }
-//                    self.selectedImage.removeAll()
-//                }
-//            default:
-//                print("Invalid Index")
-//            }
-//        } else {
-//            print("cards flipped")
-//        }
-//    }
-//
-//    @IBAction func playButtonSelected(_ sender: UIButton) {
-//        // Checking to see if the play button was selected then which device the user is using.
-//        if sender.titleLabel?.text == "Play"{
-//
-//            switch UIDevice.current.userInterfaceIdiom {
-//            // If the button was play and the user is using a phone and allowing user interaction
-//            case .phone:
-//                for image in imageViewCollection[...numImages]{
-//                    image.isUserInteractionEnabled = true
-//                    image.backgroundColor = UIColor.init(displayP3Red: 0.63, green:0.86, blue:1.00, alpha:1.0)
-//                    // Getting a random index setting the imageView image to a random image based on the index.
-//                    // Adding to the imageArray and removing the image from the iPhoneImages.
-//                    let index:Int = Int(arc4random_uniform(UInt32(iPhoneImages.count)))
-//                    image.image = iPhoneImages[index]
-//                    imageArray.append(iPhoneImages[index])
-//                    iPhoneImages.remove(at: index)
-//                }
-//                // Setting the iPhoneImages array to the imageArray.
-//                iPhoneImages = imageArray
-//            // If the user is using an iPad then looping through the collection adding the random image and removing from iPadImages array.
-//            case .pad:
-//                for image in imageViewCollection[...numImages]{
-//                    image.backgroundColor = UIColor.init(displayP3Red: 0.63, green:0.86, blue:1.00, alpha:1.0)
-//                    image.isUserInteractionEnabled = true
-//                    let index: Int = Int(arc4random_uniform(UInt32(iPadImages.count)))
-//                    image.image = iPadImages[index]
-//                    imageArray.append(image.image!)
-//                    iPadImages.remove(at: index)
-//                }
-//                // Setting the iPadImages to the imageArray.
-//                iPadImages = imageArray
-//            default:
-//                print("Device unspecified.")
-//            }
-//            // Playing countdown audio and setting the buttons title to stop.
-//            playAudio(resource: "countdown", type: "mp3")
-//            sender.setTitle("Stop", for: .normal)
-//
-//            // If the title equals stop then looping through the image collection and setting the images to nil and the backgroung color to blue.
-//        } else if sender.titleLabel?.text == "Stop"{
-//            for image in imageViewCollection{
-//                image.image = nil
-//                image.isUserInteractionEnabled = false
-//                image.backgroundColor = UIColor.init(displayP3Red: 0.63, green:0.86, blue:1.00, alpha:1.0)
-//            }
-//
-//            // Stopping the audio, invalidating the timer and resetting variables and labels, then setting the button title to Play.
-//            player.stop()
-//            timer.invalidate()
-//            timeSeconds = 0
-//            timeMinutes = 0
-//            time = 0
-//            moves = 0
-//            selectedImage = []
-//            imageArray = []
-//            movesLabel.text = ""
-//            timerLabel.text = ""
-//            sender.setTitle("Play", for: .normal)
-//        }
-//    }
-//
-//    // When the audio is finished playing setting the imageviews images to nil.
-//    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-//        for image in imageViewCollection{
-//            image.image = nil
-//        }
-//
-//        // If the timer is not already valid then creating the new timer.
-//        if timer.isValid == false{
-//            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
-//        }
-//    }
-//
-//    // Recieving the passed in strings getting teh path to the audio creating url passing the url to the audio player.
-//    func playAudio(resource: String, type: String){
-//        if let path = Bundle.main.path(forResource: resource, ofType: type){
-//            do{
-//                let url = URL(fileURLWithPath: path)
-//                player = try AVAudioPlayer(contentsOf: url)
-//
-//                // If the resource countdown assigning the delegate to the player.
-//                if resource == "countdown"{
-//                    player.delegate = self
-//                }
-//                // Preparing the player to play
-//                player.prepareToPlay()
-//            } catch{
-//                print(error.localizedDescription)
-//            }
-//            // Playing the player.
-//            player.play()
-//        }
-//    }
+    // Function for when a card is selected.
+    @IBAction func cardSelected(_ sender: UIButton) {
+        
+        guard let card = sender as? Card else {return}
+        
+        if card.currentImage == card.backImage {
+            
+            if let cardSelected = selectedCard {
+               // There is already a card selected
+                flipFront(card: card)
+                card.isUserInteractionEnabled = false
+                moves += 1
+                movesLabel.text = "Moves: \(moves)"
+                
+                DispatchQueue.main.asyncAfter(wallDeadline: .now() + 0.5) {
+                   
+                    if card.currentImage == cardSelected.cardImage {
+                        self.playAudio(resource: "correct", type: "wav")
+                        
+                        card.isUserInteractionEnabled = true
+                        cardSelected.isHidden = true
+                        card.isHidden = true
+                        self.selectedCard = nil
+                        // TODO: Check if they are a the last two matches.
+                        
+                    } else {
+                        self.playAudio(resource: "incorrect", type: "wav")
+
+                        cardSelected.isUserInteractionEnabled = true
+                        card.isUserInteractionEnabled = true
+                        self.selectedCard = nil
+                        self.flipBack(card: cardSelected)
+                        self.flipBack(card: card)
+                    }
+                }
+            } else {
+                // There is no card previously selected
+                playAudio(resource: "tap", type: "mp3")
+                flipFront(card: card)
+
+                selectedCard = card
+                card.isUserInteractionEnabled = false
+            }
+        } else {
+            print("Card is already flipped.")
+        }
+    }
+    
+
 //
 //    // Alert function
 //    func alert(){
@@ -346,15 +249,6 @@ class GameViewController: UIViewController {
 //        }
 //    }
 //
-//    // Function to keep track of the time that has passed counter the interval each time it is fired and displaying the time in the label.
-//    @objc func updateTimer(){
-//        time += Int(timer.timeInterval)
-//
-//         timeMinutes = time / 60 % 60
-//         timeSeconds = time % 60
-//
-//        timerLabel.text = String(format:"%02i:%02i", timeMinutes, timeSeconds)
-//    }
 //
 //    // Function to save CoreData.
 //    func save(){
@@ -403,7 +297,7 @@ extension GameViewController: AVAudioPlayerDelegate {
     
     // Audio CallBacks
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
-        // TODO Flip cards back.
+        
         for card in cardCollection {
             flipBack(card: card)
         }
@@ -412,7 +306,6 @@ extension GameViewController: AVAudioPlayerDelegate {
         if timer.isValid == false{
             timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
         }
-        
     }
     
     // Recieving the passed in strings getting teh path to the audio creating url passing the url to the audio player.
